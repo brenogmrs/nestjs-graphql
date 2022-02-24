85e76fe0adac8d1fafb667d96281c4199d2ab305
Breno Guimarães <brenoaraujo@outlook.com.br>
2022-02-24 16:47:28 -0300

    feat: add players controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PlayersController } from './players/players.controller';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
-  controllers: [],
-  providers: [],
+    imports: [PlayersModule],
+    controllers: [PlayersController],
+    providers: [],
 })
 export class AppModule {}
---
+++ src/players/players.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('api/v1/players')
+export class PlayersController {
+    @Post()
+    async createOrUpdatePlayer() {
+        return JSON.stringify({
+            name: 'name',
+        });
+    }
+}
