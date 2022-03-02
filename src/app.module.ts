fd410305cf0160594bb6163a779851ab60e98e1e
Breno Guimarães <brenoaraujo@outlook.com.br>
2022-03-02 13:08:48 -0300

    feat: add mongo atlas connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-    imports: [PlayersModule],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(String(process.env.MONGODB_CONN_STRING)),
+        PlayersModule,
+    ],
     controllers: [],
     providers: [],
 })
