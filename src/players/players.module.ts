61d15cac62c34a9e698c698b3727906c1db0a21b
Breno Guimarães <brenoaraujo@outlook.com.br>
2022-03-02 13:09:41 -0300

    feat: add player schema to player module

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
-import { PlayersController } from './players.controller';
-import { PlayersService } from './players.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PlayersController } from './controllers/players.controller';
+import { PlayersService } from './services/players.service';
+import { PlayerSchema } from './schemas/player.schema';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: 'Player', schema: PlayerSchema }]),
+    ],
     controllers: [PlayersController],
     providers: [PlayersService],
 })
