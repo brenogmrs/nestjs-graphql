5dcc3abfaa3d8ba383787cd2d3110424c2501cb1
Breno Guimarães <brenoaraujo@outlook.com.br>
2022-03-03 10:10:09 -0300

    chore: add validation decorators to dto

--- src/players/dtos/create-player.dto.ts
+++ src/players/dtos/create-player.dto.ts
@@ -1,11 +1,22 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class CreatePlayerDTO {
+    @IsNotEmpty()
     readonly phoneNumber: string;
+
+    @IsEmail()
     readonly email: string;
+
+    @IsNotEmpty()
     readonly name: string;
 }
 
 export class UpdatePlayerDTO {
+    @IsString()
     readonly phoneNumber?: string;
+
+    @IsString()
     readonly email?: string;
+
+    @IsString()
     readonly name?: string;
 }
