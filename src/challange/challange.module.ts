e650aa305361c6f4760e3a12e7b443f3c65162fc
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-03-09 15:06:32 -0300

    chore: add modules to be used in the service

--- src/challange/challange.module.ts
+++ src/challange/challange.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoryModule } from '../category/categories.module';
+import { PlayerModule } from '../player/player.module';
 import { ChallangeController } from './controllers/challange.controller';
 import { ChallengeSchema } from './schemas/challenge.schema';
 import { ChallangeService } from './services/challange.service';
@@ -9,6 +11,8 @@ import { ChallangeService } from './services/challange.service';
         MongooseModule.forFeature([
             { name: 'Challenge', schema: ChallengeSchema },
         ]),
+        PlayerModule,
+        CategoryModule,
     ],
     controllers: [ChallangeController],
     providers: [ChallangeService],
