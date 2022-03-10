8e059d202c1376828dd06fc64b8501ff2e42bf7c
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-03-10 14:20:28 -0300

    feat: assign default timezone to america/sao_paulo

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { formatInTimeZone } from 'date-fns-tz';
 import { AppModule } from './app.module';
 import { AllExceptions } from './common/filters/http-exception.filter';
 
@@ -6,6 +7,14 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.useGlobalFilters(new AllExceptions());
+
+    Date.prototype.toJSON = function (): any {
+        return formatInTimeZone(
+            this,
+            'America/Sao_Paulo',
+            'yyyy-MM-dd HH:mm:ssXXX',
+        );
+    };
 
     app.setGlobalPrefix('api');
     await app.listen(8080);
